b39bc9a1b599b8519a8b2fef41cfa3058fc4733c
LuonggNguyen <87462814+LuonggNguyen@users.noreply.github.com>
2022-05-12 00:24:58 +0700

    sua main

--- app/src/main/java/com/example/shopee/MainActivity.java
+++ app/src/main/java/com/example/shopee/MainActivity.java
@@ -2,16 +2,40 @@ package com.example.shopee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.shopee.views.LoginActivity;
+import com.example.shopee.views.SignUpActivity;
 
 public class MainActivity extends AppCompatActivity {
+    TextView dangNhap,dangKy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dangKy = findViewById(R.id.btnDangKy);
+        dangNhap = findViewById(R.id.btnDangNhap);
+
+        dangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        dangKy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
